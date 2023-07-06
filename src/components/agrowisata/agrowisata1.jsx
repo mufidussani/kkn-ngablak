cd3e990d6c72b5b700107e6e24fb200fa276ce04
mufidussani <sanimufidus@gmail.com>
2023-07-06 21:18:10 +0700

    fix: content agro

--- src/components/agrowisata/agrowisata1.jsx
+++ src/components/agrowisata/agrowisata1.jsx
@@ -64,7 +64,7 @@ const Agrowisata1 = () => {
         >
           <div className="font-black pointer-events-none">
             <div className="mt-1 justify-center sm:gap-1.5 md:gap-1 xl:gap-1 mobile:gap-1.5">
-              <div className="text-[#545B77] font-Satisfy text-2xl xl:text-4xl font-bold text-center mb-6">
+              <div className="text-[#545B77] font-Satisfy text-2xl sm:text-3xl xl:text-4xl font-bold text-center mb-6">
                 Rute Pendakian Gunung Andong
               </div>
             </div>
@@ -73,27 +73,28 @@ const Agrowisata1 = () => {
             Telusuri lebih lanjut tentang apa yang kami tawarkan.
           </div> */}
           <div className="container flex-col xl:flex-row flex items-center">
-            <div className="transform hover:scale-[110%] pb-4 xl:pb-0 px-4">
+            <div className="transform hover:transition-all hover:scale-[110%] pb-4 xl:pb-0 px-4">
               <img className="" src={gambar1} alt="rectangle"></img>
             </div>
-            <div className="transform hover:scale-[110%] pb-4 xl:pb-0 px-4">
+            <div className="transform hover:transition-all hover:scale-[110%] pb-4 xl:pb-0 px-4">
               {/* <h3>2</h3> */}
               <img className="" src={gambar2} alt="rectangle"></img>
             </div>
-            <div className="transform hover:scale-[110%] pb-4 xl:pb-0 px-4">
+            <div className="transform hover:transition-all hover:scale-[110%] pb-4 xl:pb-0 px-4">
               {/* <h3>2</h3> */}
               <img className="" src={gambar3} alt="rectangle"></img>
             </div>
           </div>
         </div>
-        <div className="text-justify flex-col flex xl:flex-row">
-          <div className="text-[#545B77] sm:text-base xl:text-xl">
-            Destinasi Gunung Andong memiliki tiga jalur pendakian. Setiap jalur
-            pendakian memiliki karakteristik yang berbeda. Dalam satu lokasi,
-            anda bisa mendapatkan tiga pengalaman yang berbeda tergantung dari
-            rute pendakian yang anda pilih.
+        <div className="text-justify flex-col flex xl:flex-row xl:mt-10 items-center justify-center">
+          <div className="text-[#545B77] sm:text-base xl:text-xl xl:w-2/3 mr-10">
+            Destinasi Gunung Andong memiliki tiga jalur pendakian.
+            <br />
+            Setiap jalur pendakian memiliki karakteristik yang berbeda. Dalam
+            satu lokasi, anda bisa mendapatkan tiga pengalaman yang berbeda
+            tergantung dari rute pendakian yang anda pilih.
           </div>
-          <div className="py-4 xl:pb-0">
+          <div className="xl:pb-0 items-center justify-center flex">
             {/* <h3>2</h3> */}
             <img className="" src={map_muncak} alt="rectangle"></img>
           </div>
